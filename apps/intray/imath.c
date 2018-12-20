e274280f8adb4cb4651b9ccd798c5475e8a7457e
jaggies <jaggiesjim@gmail.com>
2018-12-19 23:00:29 -0800

    Fix bug with 32-bit sqrt()

--- apps/intray/imath.c
+++ apps/intray/imath.c
@@ -4,20 +4,19 @@
  *  Created on: Dec 9, 2018
  *      Author: jmiller
  */
-
+#include <stdio.h>
 #include "imath.h"
-
-static int init = 0;
-static fixed sqrtFrac;
 
 // Quick integer square rooter
 fresult isqrt(fresult value) {
-    long result = 0;
-    long bit = 1 << (8 * sizeof(fixed) - 2); // The second-to-top bit is set: 1 << 30 for 32 bits
+    fresult bit = 1 << (8 * sizeof(fresult) - 2);
+    if (value <= 0) {
+        return 0;
+    }
     while (bit > value) {
-        // "bit" starts at the highest power of four <= the argument.
         bit >>= 2;
     }
+    fresult result = 0;
     while (bit != 0) {
         if (value >= result + bit) {
             value -= result + bit;
@@ -30,10 +29,6 @@ fresult isqrt(fresult value) {
 }
 
 fixed fsqrt(fixed value) {
-    if (!init) {
-         sqrtFrac = toFixed(isqrt(1 << fraction));
-         init = 1;
-    }
-    return fmult(sqrtFrac, (fixed) isqrt(value)); // more accurate: isqrt(value << fraction);
+    return isqrt(value << fraction);
 }
 
