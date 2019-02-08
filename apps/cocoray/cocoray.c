d45870cc902a66eca4da25c888c38a7cc9c4842b
jaggies <jaggiesjim@gmail.com>
2019-02-08 00:18:38 -0800

    Simple error diffusion.

--- apps/cocoray/cocoray.c
+++ apps/cocoray/cocoray.c
@@ -49,24 +49,24 @@ void makeRgbPalette(const int DACBITS) {
 }
 
 uint8_t ditherRGB(int i, int j, Vec3i* color) {
-    uint8_t red = (uint8_t) (clamp(color->x, 0, c_one) >> (fraction + 1 - 8)); /* 0..255 */
+    uint8_t red = (uint8_t) (color->x >> (fraction + 1 - 8)); /* 0..255 */
     red = dither(8, RBITS, (uint8_t) i, (uint8_t) j, red);
-    uint8_t grn = (uint8_t) (clamp(color->y, 0, c_one) >> (fraction + 1 - 8)); /* 0..255 */
+    uint8_t grn = (uint8_t) (color->y >> (fraction + 1 - 8)); /* 0..255 */
     grn = dither(8, GBITS, (uint8_t) i, (uint8_t) j, grn);
-    uint8_t blu = (uint8_t) (clamp(color->z, 0, c_one) >> (fraction + 1 - 8)); /* 0..255 */
+    uint8_t blu = (uint8_t) (color->z >> (fraction + 1 - 8)); /* 0..255 */
     blu = dither(8, BBITS, (uint8_t) i, (uint8_t) j, blu);
     return (red << (GBITS + BBITS)) | (grn << BBITS) | blu;
 }
 
 static Vec3i palette[16]; // 16-color table entries
 
-void makeOptimalPalette() {
+void makeSimplePalette() {
     for (uint8_t i = 0; i < 4; i++) {
         int val = i << 6;
-        ivec3(val, 0, 0, &palette[i]);
-        ivec3(0, val, 0, &palette[i + 4]);
-        ivec3(0, 0, val, &palette[i + 8]);
-        ivec3(val, val, val, &palette[i + 12]);
+        ivec3(val, 0, 0, &palette[i]); // red
+        ivec3(0, val, 0, &palette[i + 4]); // green
+        ivec3(0, 0, val, &palette[i + 8]); // blue
+        ivec3(val, val, val, &palette[i + 12]); // gray
     }
 
     for (uint8_t i = 0; i < 16; i++) {
@@ -78,21 +78,49 @@ void makeOptimalPalette() {
 }
 
 uint8_t nearest(Vec3i* color) {
-    uint16_t best = 0xffff;
     uint8_t index = 0;
+    const uint8_t shift = (fraction + 1 - 8);
+    Vec3i scaled;
+    scaled.x = color->x >> shift; /* 0..255 */
+    scaled.y = color->y >> shift; /* 0..255 */
+    scaled.z = color->z >> shift; /* 0..255 */
+    fixed best = 0x7fff; // TODO: base on sizeof(fixed)
     for (uint8_t i = 0; i < 16; i++) {
-        Vec3i v;
-        v.x = clamp(color->x, 0, c_one) >> (fraction + 1 - 8); /* 0..255 */
-        v.y = clamp(color->y, 0, c_one) >> (fraction + 1 - 8); /* 0..255 */
-        v.z = clamp(color->z, 0, c_one) >> (fraction + 1 - 8); /* 0..255 */
-        isub3(&v, &palette[i], &v);
-        imult3s(&v, c_half, &v); // avoid overflow into sign bit
-        fixed dist = idot3(&v, &v);
+        Vec3i diff;
+        isub3(&scaled, &palette[i], &diff);
+        imult3s(&diff, c_half, &diff); // avoid overflow into sign bit
+        fixed dist = idot3(&diff, &diff);
         if (dist < best) {
             index = i;
             best = dist;
         }
     }
+    return index;
+}
+
+static Vec3i error;
+uint8_t diffusion(Vec3i* color, int reset) {
+    if (reset) {
+        error.x = error.y = error.z = c_zero;
+    }
+    const uint8_t shift = (fraction + 1 - 8);
+    Vec3i adjColor;
+    iadd3(color, &error, &adjColor);
+    Vec3i clamped;
+    clamped.x = clamp(adjColor.x, 0, c_one);
+    clamped.y = clamp(adjColor.y, 0, c_one);
+    clamped.z = clamp(adjColor.z, 0, c_one);
+
+    uint8_t index = nearest(&clamped);
+    Vec3i chosen;
+    chosen.x = palette[index].x << shift;
+    chosen.y = palette[index].y << shift;
+    chosen.z = palette[index].z << shift;
+    isub3(color, &chosen, &chosen);
+    iadd3(&error, &chosen, &error);
+    error.x = clamp(error.x, -c_one, c_one);
+    error.y = clamp(error.y, -c_one, c_one);
+    error.z = clamp(error.z, -c_one, c_one);
     return index;
 }
 
@@ -105,7 +133,7 @@ int main(int argc, char** argv) {
     /* Graphics */
     hscreen(GFXMODE);
     //makeRgbPalette(DACBITS);
-    makeOptimalPalette();
+    makeSimplePalette();
 
     /* Create the scene */
     float aspect = (float) WIDTH / HEIGHT;
@@ -127,8 +155,12 @@ int main(int argc, char** argv) {
             } else {
                 color = scene->background;
             }
+            color.x = clamp(color.x, 0, c_one);
+            color.y = clamp(color.y, 0, c_one);
+            color.z = clamp(color.z, 0, c_one);
             //hset(i, j, ditherRGB(i, j, &color));
-            hset(i, j, nearest(&color));
+            //hset(i, j, nearest(&color));
+            hset(i, j, diffusion(&color, i == 0));
         }
     }
 
