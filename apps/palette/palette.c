e7a921ba4bc78466e2224845cd4bae578a0292fc
jaggies <jaggiesjim@gmail.com>
2019-02-14 02:25:30 -0800

    Reset palette index on vertical sync

--- apps/palette/palette.c
+++ apps/palette/palette.c
@@ -13,6 +13,9 @@ const char GFXMODE = HS320x192x16;
 uint8_t *irqVector = * (uint8_t **) 0xFFF8;
 uint8_t *firqVector = * (uint8_t **) 0xFFF6;
 uint8_t *hsyncCtrl = (uint8_t *) 0xff01;
+uint8_t *vsyncCtrl = (uint8_t *) 0xff03;
+uint8_t *init0 = (uint8_t *) 0xff90;
+uint8_t *firqEn = (uint8_t *) 0xff93;
 uint8_t *palCtrl = (uint8_t *) 0xffb0; // entry of palette 0
 
 // Enables 6309 Native mode for higher speed
@@ -24,15 +27,28 @@ void set6309Native() {
 
 static uint8_t clr;
 
-interrupt void twiddle() {
+interrupt void horizontalISR() {
     *palCtrl = clr++;
 }
 
-void enableHzIrq() {
+interrupt void verticalISR() {
+    clr = 0;
+}
+
+void enableVideoIRQs() {
     disableInterrupts();
+
+    // Horizontal setup
     *hsyncCtrl = 0xf7; // enable Horizontal IRQ
     *irqVector = 0x7E;  // extended JMP extension
-    *(void **) (irqVector + 1) = (void *) twiddle;
+    *(void **) (irqVector + 1) = (void *) horizontalISR;
+
+    // Vertical setup
+    *firqVector = 0x7E;  // extended JMP extension
+    *(void **) (firqVector + 1) = (void *) verticalISR;
+    *init0 |= 0x10; // GIME FIRQ output enabled
+    *firqEn = 0x08; // vertical boarder firq enabled
+
     enableInterrupts();
 }
 
@@ -44,7 +60,7 @@ int main(int argc, char** argv) {
 
     /* Graphics */
     hscreen(GFXMODE);
-    enableHzIrq();
+    enableVideoIRQs();
 
     while (1)
         ;
