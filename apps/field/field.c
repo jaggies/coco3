9bf536c37a3d01ee3bff489d057ebc9a532cad68
jaggies <jaggiesjim@gmail.com>
2019-02-20 02:46:46 -0800

    Remember to clear the damn IRQs
    The emulator seems to just look for an edge. As a consequence,
    the real-world CoCo gets into an infinite loop in the ISR. Oops.

--- apps/field/field.c
+++ apps/field/field.c
@@ -11,20 +11,27 @@
 #include "cc3gfx.h"
 
 const char GFXMODE = HS320x192x16;
-int field = 0;
+uint8_t field = 0;
 
 interrupt void verticalISR() {
     for (uint8_t i = 0; i < 16; i++) {
-        paletteRGB(i, field == 0 ? i : 0, field == 1 ? i : 0, field == 2 ? i : 0);
+        uint8_t v = i & 3;
+        uint8_t r = field == 0 ? v : 0;
+        uint8_t g = field == 1 ? v : 0;
+        uint8_t b = field == 2 ? v : 0;
+        palCtrl[i] = toPalette(r, g, b);
     }
     field = field < 2 ? (field+1) : 0;
+    uint8_t x = *irqEn; // clear irqs
+    uint8_t y = *firqEn;
 }
 
 void enableVideoIRQs() {
     disableInterrupts();
-    *init0 |= 0x30; // GIME FIRQ and IRQ output enabled
-    *irqEn |= 0x08; // vertical irq enabled
-    setIrq(verticalISR);
+    *init0 = 0x5c; // GIME CC3, MMU, DRAM, 16k/16k ROM, FIRQ_en
+    *irqEn = 0x00; // DISABLE IRQs
+    *firqEn = 0x08; // FIRQ for vertical ISR
+    setFirq(verticalISR);
     enableInterrupts();
 }
 
