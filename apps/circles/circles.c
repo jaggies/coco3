b8068ed00e302f65b642727042f5730c2df9e180
jaggies <jaggiesjim@gmail.com>
2019-04-07 10:24:32 -0700

    Test case with both random and increasing circles
    Built-in random number generator is crap, so mix up the bits a little.

--- apps/circles/circles.c
+++ apps/circles/circles.c
@@ -14,6 +14,8 @@
 #define HEIGHT 225
 #define DEPTH 4
 
+#define N 20
+
 void simpleRGB() {
     for (uint8_t i = 0; i < 16; i++) {
         uint8_t red = (i & 1) ? 3 : 0;
@@ -21,6 +23,10 @@ void simpleRGB() {
         uint8_t blu = ((i >> 3) & 1) ? 3 : 0;
         setPalette(i, red, grn, blu);
     }
+}
+
+int16_t myrandom() {
+    return (rand() >> 1) ^ rand();
 }
 
 int main(int argc, char** argv) {
@@ -38,9 +44,20 @@ int main(int argc, char** argv) {
     const int height = getHeight();
     const int width = getWidth();
 
-    uint8_t clr = 1;
+    int count = 1000;
+    while (count--) {
+        int16_t xc = myrandom() % width;
+        int16_t yc = myrandom() % height;
+        int16_t r = myrandom() % (width>>2);
+        uint8_t clr = (uint8_t) (myrandom());
+        circle(xc, yc, r, clr);
+    }
 
-    while (1) {
+    clear(0x8); // blue
+
+    uint8_t clr = 1;
+    count = 256;
+    while (count--) {
         int16_t xc = width/2;
         int16_t yc = height/2;
         circle(xc, yc, clr, clr++);
