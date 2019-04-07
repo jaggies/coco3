8ac1262f215e42ece50d0e138d5aecd29676b224
jaggies <jaggiesjim@gmail.com>
2019-04-06 19:04:03 -0700

    inline set method for about 12% speed improvement.

--- lib/cc3/cc3line.c
+++ lib/cc3/cc3line.c
@@ -6,6 +6,7 @@
  */
 
 #include "os.h"
+#include "cc3hw.h"
 #include "cc3gfx.h"
 #include "cc3util.h"
 #include "cc3raster.h"
@@ -14,20 +15,28 @@ extern GfxState gfx;
 
 void line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t clr) {
     int16_t stepX = x0 < x1 ? 1 : -1;
-    int16_t stepY = y0 < y1 ? (int16_t) gfx.bytes_per_row : -(int16_t)gfx.bytes_per_row;
-    const int16_t dx = abs(x1-x0);
-    const int16_t dy = abs(y1-y0);
+    int16_t stepY = y0 < y1 ? (int16_t) gfx.bytes_per_row : -(int16_t) gfx.bytes_per_row;
+    const int16_t dx = abs(x1 - x0);
+    const int16_t dy = abs(y1 - y0);
 
     rasterPos(x0, y0);
     rasterColor(clr);
     int count = max(dx, dy);
     int16_t err = dx - dy;
+    disableInterrupts();
     do {
-        rasterSet();
+        const uint16_t offset = gfx.base_y_offset + (gfx.rasterX >> 1); // TODO: other bit depths
+        const uint8_t oldPage = *PAGE_SELECT;
+
+        *PAGE_SELECT = gfx.base_page + (uint8_t) (offset >> PAGE_BITS);
+        uint8_t* ptr = (uint8_t*) PAGE_WINDOW + (offset & 0x1fff);
+        *ptr = (*ptr & (~gfx.pixel_mask)) | (gfx.color & gfx.pixel_mask);
+        *PAGE_SELECT = oldPage;
+
         int16_t e2 = err << 1;
-        if (e2 <  dx) {
-           err += dx;
-           gfx.base_y_offset += stepY; // inline rasterInc/DecY
+        if (e2 < dx) {
+            err += dx;
+            gfx.base_y_offset += stepY; // inline rasterInc/DecY
         }
         if (e2 > -dy) {
             err -= dy;
@@ -35,6 +44,5 @@ void line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t clr) {
             gfx.pixel_mask = ~gfx.pixel_mask; // inline rasterInc/DecX
         }
     } while (count--);
+    enableInterrupts();
 }
-
-
