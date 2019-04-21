6a2c68a48fc03e715bfc8240cba475e7fcae27e9
jaggies <jaggiesjim@gmail.com>
2019-04-21 03:11:11 -0700

    Slightly more efficient rect() code.

--- lib/cc3/cc3rect.c
+++ lib/cc3/cc3rect.c
@@ -15,15 +15,18 @@
 void rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1, bool fill)
 {
     if (fill) {
-        int16_t xmin = min(x0, x1);
-        int16_t xmax = max(x0, x1);
-        int16_t ymin = min(y0, y1);
-        int16_t ymax = max(y0, y1);
+        if (x0 > x1) {
+            int16_t tmp = x0; x0 = x1; x1 = tmp;
+        }
+        if (y0 > y1) {
+            int16_t tmp = y0; y0 = y1; y1 = tmp;
+        }
+        int16_t dx = x1 - x0;
         do {
-            rasterPos(xmin, ymin);
-            rasterSpan(xmax - xmin);
-            ymin++;
-        } while (ymin < ymax);
+            rasterPos(x0, y0);
+            rasterSpan(dx);
+            y0++;
+        } while (y0 < y1);
     } else { // Draw outline
         line(x0, y0, x1, y0);
         line(x0, y1, x1, y1);
