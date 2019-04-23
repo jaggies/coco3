0db748124e1282e57da86ee52f407247714c9c6b
jaggies <jaggiesjim@gmail.com>
2019-04-23 02:39:20 -0700

    Always render lines in positive Y direction to agree with triangle rendering
    Benchmark ngon: 66.9s

--- lib/cc3/cc3line.c
+++ lib/cc3/cc3line.c
@@ -14,10 +14,15 @@
 extern GfxState gfx;
 
 void line(int16_t x0, int16_t y0, int16_t x1, int16_t y1) {
+    // Always render in positive Y direction
+    if (y0 > y1) {
+        int16_t tmp = x0; x0 = x1; x1 = tmp;
+        tmp = y0; y0 = y1; y1 = tmp;
+    }
     const int16_t stepX = x0 < x1 ? 1 : -1;
-    const int16_t stepY = y0 < y1 ? (int16_t) gfx.bytes_per_row : -(int16_t) gfx.bytes_per_row;
+    const int16_t stepY = gfx.bytes_per_row; // always incrementing
     const int16_t dx = abs(x1 - x0);
-    const int16_t dy = -abs(y1 - y0);
+    const int16_t dy = y0 - y1; // dy is always negative
 
     rasterPos(x0, y0);
     int count = max(dx, -dy);
